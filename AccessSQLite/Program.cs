3c97469438b3ef0890cb3975ed317141b3baac9b
R.H. Stine <bob@waltonstine.net>
2017-10-28 07:37:08 -0400

    Start with a Console app rather than a library and xunit tests.

---
+++ AccessSQLite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace waltonstine.demo.dotnet.sqlite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
