6709665b82b1dd254d0eaae29edf48ed466983ac
R.H. Stine <bob@waltonstine.net>
2017-10-28 08:14:10 -0400

    Open demo.db and insert a row into Parent.

--- AccessSQLite/Program.cs
+++ AccessSQLite/Program.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using SQLite;
+using System;
 
 namespace waltonstine.demo.dotnet.sqlite
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Parent 
         {
-            Console.WriteLine("Hello World!");
+            [PrimaryKey, AutoIncrement]
+            public int    Parent_pk { get; set; }
+            public string Name      { get; set; }
+            public float  Pi        { get; set; }
+            public byte[] Data      { get; set; }
+        }
+
+
+        static private int InsertParent(SQLiteConnection conn, string name, float pi, byte[] blob)
+        {
+            int insertCnt = conn.Insert(new Parent()
+            {
+                Name = name,
+                Pi = pi,
+                Data = blob
+            });
+
+            return insertCnt;
+        }
+
+        static public void Main(string[] args)
+        {
+            string dbPath =  (args.Length == 0) ? "../demo.db" : args[0];
+
+            Console.WriteLine($"Open database {dbPath}");
+
+            SQLiteConnection conn = new SQLiteConnection(dbPath);
+
+            int cnt = InsertParent(conn, "george", 1.234F, null);
+
+            conn.Close();
+
+            Console.WriteLine($"Rows inserted: {cnt}");
         }
     }
 }
